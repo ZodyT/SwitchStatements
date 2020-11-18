d698ed7bae8472597b06526ea4e8ffcb968b996f
ZodyT <Zoraida1988@yahoo.com>
2020-11-18 12:07:55 -0600

    Finished

--- SwitchStatements/Program.cs
+++ SwitchStatements/Program.cs
@@ -6,12 +6,6 @@ namespace SwitchStatements
     {
         static void Main(string[] args)
         {
-            //Ask the user for their favorite school subject.
-            //Store their answer in a variable
-            //Pass the variable as the expression in a switch statement.
-            //Create 5 different cases for different subjects.  
-            //Each case will print the subject chosen and a custom message of your choosing.
-            //Create a default case to handle any case not handled.
             Console.WriteLine("What is your favorite school subject?");
             var Subject = Console.ReadLine().ToLower();
 
@@ -24,7 +18,7 @@ namespace SwitchStatements
                     Console.WriteLine($"You picked { Subject}, you must read alot.");
                     break;
                 case "chemistry":
-                    Console.WriteLine($"You picked { Subject}, you must know all the elements");
+                    Console.WriteLine($"You picked { Subject}, you must know all the elements.");
                     break;
                 case "biology":
                     Console.WriteLine($"You picked { Subject}, you must have studied the human body.");
@@ -35,8 +29,6 @@ namespace SwitchStatements
                 default:
                     Console.WriteLine("I don't know that subject.");
                     break;
-
-
             }
         }
     }
